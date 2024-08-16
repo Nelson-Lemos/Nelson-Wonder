5a59c30647771c30369417b86a26b51f8eee8cbd
Nelson Lemos <165869240+Nelson-Lemos@users.noreply.github.com>
2024-08-16 06:12:03 +0100

    alterador_9.js
    projecto alterador de texto

--- alterador_9.js
+++ alterador_9.js
@@ -1,20 +1,38 @@
+
 console.log("o javascript esta rodando")
 
 
+let  btn=document.querySelector("#btn")
 
-function btn() {
+ btn.addEventListener("click",()=>{ 
     const input=document.querySelector("#text-input");
     const input1=document.querySelector("#replace-input");
     const input3=document.querySelector("#replace-with-input");
     
-
-    if(input.value.includes(input1.value)){
-        input.value.replace(/javascript/gi,input1.value,input3.value);
-        input.value=input3.value
-
-
-      const sect=document.querySelector("#sect");
-       sect.innerHTML=" "
-       sect.innerHTML=`<Selection id="sect"><p id="h1">${input.value}</p></Selection>`
+    if(input.value==" "){
+        alert("Não pode começar com espaço");
     }
-}
+
+    else if(!input.value){
+        alert("digite um texo para alterar")
+    }
+    else if(!input1.value){
+        alert("o espaço da subistituição está vazio")
+        
+            }
+
+            setInterval(()=>{
+                
+    if(input.value.includes(input1.value)){
+        const sect=document.querySelector("#sect");
+            sect.value=input.value.replaceAll (input1.value,input3.value);
+            console.log(sect.value)
+       sect.innerHTML=`<Selection id="sect"><p id="h1">${sect.value}</p></Selection>`
+    }
+
+            },1000)
+
+    if(!input3.value){
+        alert("digite algo para alterar o texto");
+    }
+})
